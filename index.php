57921493293e3b16127c0cd1631e7588ef027ed5
jrrj67 <ricardojunior.6767@gmail.com>
2020-03-01 14:12:14 -0300

    Start

---
+++ index.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/materialize.css">
+    <link rel="stylesheet" href="css/nouislider.css">
+    <title>Visualizador de border-radius</title>
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col s12 m6 push-m3">
+            <div class="card">
+                <div class="card-title z-depth-2">
+                    <div id="test-slider"></div>
+                </div>
+                <div class="card-content center">
+                    <img class="br hoverable z-depth-2" src="images/square.png" width="220px" alt="">
+                </div>
+                <div class="card sticky-action">
+                    <div id="test-slider1"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<script src="js/materialize.js"></script>
+<script src="js/jquery-3.4.1.min.js"></script>
+<script src="js/wNumb.js"></script>
+<script src="js/nouislider.js"></script>
+</body>
+</html>
